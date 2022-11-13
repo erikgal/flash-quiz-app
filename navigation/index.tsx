60fb97883cf1776e661bf8f82c35a6ea398f5beb
Erik Galler <43548211+erikgal@users.noreply.github.com>
2022-11-14 00:42:55 +0100

    fixed white screen loading if auth persistent state does not exsist

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useAuthentication } from '../utils/hooks/useAuthentication'
 import UserStack from './UserStack'
 import AuthStack from './AuthStack'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { View } from 'react-native'
 
 const RootNavigation = (): JSX.Element => {
+  const [loading, setLoading] = useState(true)
   const { user } = useAuthentication()
 
-  return (user != null) ? <UserStack /> : <AuthStack />
+  useEffect(() => {
+    async function checkIfLoggedIn (): Promise<void> {
+      const allKeys = await AsyncStorage.getAllKeys()
+      const authKey = allKeys.filter(item => item.startsWith('firebase:authUser'))
+      if (authKey.length === 0) {
+        setLoading(false)
+      }
+    }
+    void checkIfLoggedIn()
+  }, [])
+
+  useEffect(() => {
+    if (user != null) {
+      setLoading(false)
+    }
+  }, [user])
+
+  return (user != null) ? <UserStack /> : loading ? <View/> : <AuthStack />
 }
 
 export default RootNavigation
