8452150e85b5d45692d328a3e41438983184c87c
Erik Galler <43548211+erikgal@users.noreply.github.com>
2023-01-10 01:47:04 +0100

    mc

--- utils/functions/getNumberOfChars.ts
+++ utils/functions/getNumberOfChars.ts
@@ -1,7 +1,6 @@
 import { QuestionForm } from '../../types'
 
 export function getNumberOfChars (index: number, question: QuestionForm): number {
-  console.log(index, question)
   const inputIndexArray: number[] = []
   question.question.split(' ').forEach((word, i) => {
     if (word.includes('xxx')) {
@@ -18,7 +17,6 @@ export function getNumberOfChars (index: number, question: QuestionForm): number
 
   if (inputNumber != null) {
     const answer = question.answer[inputNumber].reduce((a, b) => (a.length > b.length ? a : b), '')
-    console.log(answer.length)
     return answer.length
   }
   console.log('ERROR: The answer corresponding to the input was not found')
